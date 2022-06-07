73c147e01d9c19ead8b1dbc50487500d2b9bf6e7
Stailer23 <stailer23@mail.ru>
2022-06-07 10:33:53 +0300

    for heroku2

--- create_bot.py
+++ create_bot.py
@@ -5,7 +5,6 @@ import os
 
 storage = MemoryStorage()
 bot = Bot(token=os.environ.get('TOKEN'))
-# bot = Bot(token='5515165760:AAE6Of-wXLuOfYNw7xeSuEfVjWP07sEV38w')
 dp = Dispatcher(bot, storage=storage)
 
 
